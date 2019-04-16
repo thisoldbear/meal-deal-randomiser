be4c8e4d9959dcd80ade0de0dd1be312f5501dd7
thisoldbear <danielwilliams.design@gmail.com>
2019-04-16 12:14:36 +0100

    Add click to hold functionality

--- src/components/item.js
+++ src/components/item.js
@@ -1,8 +1,21 @@
 import React from "react";
 
-export default ({ label, selection }) => (
-  <div className="item">
+export default ({ label, item, id, hold, isHeld }) => (
+  <div
+    className={`item ${isHeld ? "item--isHeld" : ""}`}
+    onClick={() => {
+      if (item) {
+        hold(id);
+      }
+    }}
+  >
     <h2 className="item__label">{label}</h2>
-    <span className="item__emoji">{selection ? selection : "❓"}</span>
+    <span
+      className={`item__icon ${item ? "item__icon--border" : ""} ${
+        isHeld ? "item__icon--isHeld" : ""
+      }`}
+    >
+      <span className="item__icon-emoji">{item ? item : "❓"}</span>
+    </span>
   </div>
 );
